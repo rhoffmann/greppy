09bbe56377aa75a421f63dbed73db24df13e80f3
Richard Hoffmann <rhoffmann@fastmail.com>
2023-09-26 20:23:11 +0200

    add lib

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use std::{env, error::Error, fs, process};
+use std::{env, process};
 
-struct Config {
-    query: String,
-    file_path: String,
-}
+use greppy::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,27 +13,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = greppy::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     };
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{contents}");
-    Ok(())
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
 }
